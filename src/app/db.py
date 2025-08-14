a97a5964440c6b625c839816d00e0d76b537ed2a
Dmitry Sergienko <d-sergienko@users.noreply.github.com>
2025-08-14 18:08:59 +0300

    adjust DB pool size

--- src/app/db.py
+++ src/app/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=30, max_overflow=20)
 Base = declarative_base()
 DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
